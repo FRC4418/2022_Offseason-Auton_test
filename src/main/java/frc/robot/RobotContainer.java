6bd8f986b70311d79bb2f6dd8a33558ba0b295c1
AndyHegemann <andyhegemann@gmail.com>
2022-08-14 18:39:43 -0600

    Start with working r2v2 code

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,65 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+
+import com.stuypulse.stuylib.input.Gamepad;
+import com.stuypulse.stuylib.input.gamepads.AutoGamepad;
+
+import frc.robot.constants.Ports;
+import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.Feeder;
+import frc.robot.subsystems.Shooter;
+import frc.robot.commands.AutonHalt;
+import frc.robot.commands.DrivetrainDrive;
+import frc.robot.commands.FeederControl;
+import frc.robot.commands.FeederReverse;
+import frc.robot.commands.ShooterControl;
+import frc.robot.commands.ShooterReverse;
+import edu.wpi.first.wpilibj2.command.Command;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and button mappings) should be declared here.
+ */
+public class RobotContainer {
+  // The robot's subsystems and commands are defined here...
+  private final Drivetrain drivetrain = new Drivetrain();
+  private final Shooter shooter = new Shooter();
+  private final Feeder feeder = new Feeder();
+
+  private final AutonHalt autonHalt = new AutonHalt();
+
+
+  public final Gamepad driver = new AutoGamepad(Ports.Gamepad.DRIVER);
+
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  public RobotContainer() {
+    drivetrain.setDefaultCommand(new DrivetrainDrive(drivetrain, driver));
+    shooter.setDefaultCommand(new ShooterControl(shooter, driver));
+    feeder.setDefaultCommand(new FeederControl(feeder, driver));
+    configureButtonBindings();
+  }
+
+
+  private void configureButtonBindings() {
+    driver.getRightBumper().whenHeld(new FeederReverse(feeder));
+    driver.getLeftBumper().whenHeld(new ShooterReverse(shooter));
+
+
+  }
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    // An ExampleCommand will run in autonomous
+    return autonHalt;
+  }
+}
---
+++ src/main/java/frc/robot/commands/FeederControl.java
@@ -0,0 +1,65 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import com.stuypulse.stuylib.input.Gamepad;
+import com.stuypulse.stuylib.math.SLMath;
+import com.stuypulse.stuylib.network.SmartNumber;
+import com.stuypulse.stuylib.streams.IStream;
+import com.stuypulse.stuylib.streams.filters.LowPassFilter;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.constants.Settings;
+import frc.robot.subsystems.Feeder;
+
+public class FeederControl extends CommandBase {
+
+  private final Feeder feeder;
+  private final Gamepad driver;
+  private final IStream commandedSpeed;
+  SmartNumber CURRENT_COMMAND_VALUE = new SmartNumber("Feeder current command value", 0);
+
+  /** Creates a new FeederControl. */
+  public FeederControl(Feeder feeder, Gamepad driver) {
+    this.feeder = feeder;
+    this.driver = driver;
+
+    this.commandedSpeed =
+        IStream.create(() -> driver.getRightTrigger())
+                .filtered(
+                        x -> SLMath.map(x, 0, 1, Settings.Feeder.MIN_SPEED.get(), 
+                                        Settings.Feeder.MAX_SPEED.get()
+                                        ),
+                        x -> SLMath.spow(x, Settings.Feeder.SPEED_POWER.get()),
+                        new LowPassFilter(Settings.Feeder.SPEED_FILTER)
+                        );
+    
+    addRequirements(feeder);
+    
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    System.out.println("feeder initialized");
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    feeder.setRPM(commandedSpeed.get());
+    CURRENT_COMMAND_VALUE.set(commandedSpeed.get());
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
